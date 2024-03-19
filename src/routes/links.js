925d6555ab0f3a50a8033e05ec874d792ca5b6b3
Fran99879 <fran99879@gmail.com>
2024-03-19 19:48:05 -0300

    Agg button/delete

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,8 +7,33 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('recibido');
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+    res.redirect('/links');
 });
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links');
+    res.render('links/list', { links });
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id])
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async(req, res)=> {
+    const {id}= req.params;
+    console.log(id)
+    res.send('recibido')
+    
+})
 
 module.exports = router;
